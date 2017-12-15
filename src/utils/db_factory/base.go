6fa4d1ed6ff5385e54abd81d4cc2d18281c88b66
lrs <82623629@qq.com>
2017-12-15 15:21:10 +0800

    修改了更合理的命名，及格式化代码

---
+++ src/utils/db_factory/base.go
@@ -0,0 +1,76 @@
+package db_factory
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	log "utils/logger"
+)
+
+type Config struct {
+	Host        string
+	UserName    string
+	Password    string
+	Database     string
+	MaxConn     int
+	MaxIdleConn int
+}
+
+type DBPool struct {
+	currDB *sql.DB
+	isConn bool
+}
+
+func NewDataSource(config Config) DBPool {
+	singleDB := initDb(config)
+	err := singleDB.Ping()
+	if err != nil {
+		log.Error("cannot conn db %s", err.Error())
+	}
+	return singleDB
+}
+
+func convConfig2Str(config Config) string {
+	dsn := "%s:%s@tcp(%s)/%s?charset=utf8"
+	return fmt.Sprintf(dsn, config.UserName, config.Password, config.Host, config.Database)
+}
+
+func initDb(config Config) DBPool {
+	conn_str := convConfig2Str(config)
+	db, err := sql.Open("mysql", conn_str)
+	if err != nil {
+		log.Error("openstr %s", conn_str)
+		log.Error("cannot conn db %s", err.Error())
+		return DBPool{
+			isConn: false,
+		}
+	} else {
+		db.SetMaxOpenConns(config.MaxConn)
+		db.SetMaxIdleConns(config.MaxIdleConn)
+		return DBPool{
+			currDB: db,
+			isConn: true,
+		}
+	}
+}
+
+func parseRow(r *sql.Rows) map[string]string {
+	cols, _ := r.ColumnTypes() // Remember to check err afterwards
+	values := make([]sql.RawBytes, len(cols))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+	err := r.Scan(scanArgs...)
+	if err != nil {
+		log.Error("query error %s", err.Error())
+	}
+	var item map[string]string = make(map[string]string, 0)
+	for i, col := range values {
+		if col != nil {
+			k := cols[i].Name()
+			item[k] = string(col)
+		}
+	}
+	return item
+}
---
+++ src/utils/db_factory/setter.go
@@ -0,0 +1,65 @@
+package db_factory
+
+import (
+	"database/sql"
+	log "utils/logger"
+)
+
+func (m DBPool) Query(query string, args ...interface{}) []map[string]string {
+	log.Debug("Query sql :(%s)", query)
+	rows, err := m.currDB.Query(query, args...)
+	defer rows.Close()
+	if err != nil {
+		log.Error("query error %s", err.Error())
+		return nil
+	}
+	res := make([]map[string]string, 0)
+	for rows.Next() {
+		res = append(res, parseRow(rows))
+	}
+	return res
+}
+
+func (m DBPool) QueryRow(query string, args ...interface{}) map[string]string {
+	log.Debug("Query Row sql :(%s)", query)
+	rows, err := m.currDB.Query(query, args...)
+	defer rows.Close()
+	if err != nil {
+		log.Error("query error %s", err.Error())
+		return nil
+	}
+	if rows.Next() {
+		return parseRow(rows)
+	}
+	return nil
+}
+
+func (m DBPool) Exec(query string, args ...interface{}) sql.Result {
+	log.Debug("Exec sql :(%s)", query)
+	res, err := m.currDB.Exec(query, args...)
+	if err != nil {
+		log.Error("exec error %s", err.Error())
+		return nil
+	}
+	return res
+}
+
+func (m DBPool) Prepare(query string) (*sql.Stmt, error) {
+	return m.currDB.Prepare(query)
+}
+
+func (m DBPool) GetDb() *sql.DB {
+	return m.currDB
+}
+
+func (m DBPool) Close() {
+	m.currDB.Close()
+}
+
+func (m DBPool) Ping() error {
+	return m.currDB.Ping()
+}
+
+func (m DBPool) IsConn() bool {
+	return m.isConn
+}
