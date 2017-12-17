6e053dc9c93f852d9f47c6dc916a468f5e6104e8
lideyong <lideyong@outlook.com>
2017-12-17 23:44:34 +0800

    update: add database operation method

--- src/servlets/common/db_manager.go
+++ src/servlets/common/db_manager.go
@@ -6,20 +6,21 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "utils/config"
 	"utils/logger"
+	"fmt"
+	"utils/config"
 )
 
 var gDbUser *sql.DB
 
 func DbInit() error {
-	/*
-		dsn_ := fmt.Sprint("%s:%s@tcp(%s)/%s?charset=utf8",
+
+		dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8",
 			config.GetConfig().DBUser,
 			config.GetConfig().DBUserPwd,
 			config.GetConfig().DBHost,
 			config.GetConfig().DBDatabase)
-	*/
 
-	dsn := "lvt_serv:U9D$WHTo@(10.100.15.96:3306)/livesone_user?charset=utf8"
+	//dsn := "lvt_serv:U9D$WHTo@(10.100.15.96:3306)/livesone_user?charset=utf8"
 
 	var err error
 	gDbUser, err = sql.Open("mysql", dsn)
@@ -32,13 +33,27 @@ func DbInit() error {
 	return nil
 }
 
+func ExistsUID(uid int64) bool {
+
+	return false
+}
+
+func ExistsEmail(email string) bool {
+
+	return false
+}
+
+func ExistsPhone(country int, phone string) bool {
+
+	return false
+}
+
 func InsertAccount(account Account) (int64, error) {
 	if gDbUser == nil {
 		logger.Error("database not ready")
 		return 0, nil
 	}
 
-	//stmt, err := gDbUser.Prepare("INSERT account SET uid=?, email=?, country=?, phone=?, login_password=?, " +
 	stmt, err := gDbUser.Prepare("INSERT account SET uid=?, login_password=?, " +
 		"`language`=?, region=?, `from`=?, register_time=?, update_time=?, register_type=?")
 	if err != nil {
@@ -46,15 +61,103 @@ func InsertAccount(account Account) (int64, error) {
 		return 0, err
 	}
 
-	//res, err := stmt.Exec(account.UID, account.Email, account.Country, account.Phone, account.LoginPassword,
-	res, err := stmt.Exec(account.UID, account.LoginPassword,
+	ret, err := stmt.Exec(account.UID, account.LoginPassword,
 		account.Language, account.Region, account.From, account.RegisterTime, account.UpdateTime, account.RegisterType)
 	if err != nil {
 		logger.Fatal(err)
 		return 0, err
 	}
 
-	id, _ := res.LastInsertId()
+	id, _ := ret.LastInsertId()
 
 	return id, nil
+}
+
+func InsertAccountWithEmail(account Account) (int64, error) {
+	if gDbUser == nil {
+		logger.Error("database not ready")
+		return 0, nil
+	}
+
+	stmt, err := gDbUser.Prepare("INSERT account SET uid=?, email=?, login_password=?, " +
+		"`language`=?, region=?, `from`=?, register_time=?, update_time=?, register_type=?")
+	if err != nil {
+		logger.Fatal(err)
+		return 0, err
+	}
+
+	ret, err := stmt.Exec(account.UID, account.Email, account.LoginPassword,
+		account.Language, account.Region, account.From, account.RegisterTime, account.UpdateTime, account.RegisterType)
+	if err != nil {
+		logger.Fatal(err)
+		return 0, err
+	}
+
+	id, _ := ret.LastInsertId()
+
+	return id, nil
+}
+
+func InsertAccountWithPhone(account Account) (int64, error) {
+	if gDbUser == nil {
+		logger.Error("database not ready")
+		return 0, nil
+	}
+
+	stmt, err := gDbUser.Prepare("INSERT account SET uid=?, country=?, phone=?, login_password=?, " +
+		"`language`=?, region=?, `from`=?, register_time=?, update_time=?, register_type=?")
+	if err != nil {
+		logger.Fatal(err)
+		return 0, err
+	}
+
+	ret, err := stmt.Exec(account.UID, account.Country, account.Phone, account.LoginPassword,
+		account.Language, account.Region, account.From, account.RegisterTime, account.UpdateTime, account.RegisterType)
+	if err != nil {
+		logger.Fatal(err)
+		return 0, err
+	}
+
+	id, _ := ret.LastInsertId()
+
+	return id, nil
+}
+
+func GetAccountByUID(uid string) (Account, error) {
+	var account Account
+
+
+	return account, nil
+}
+
+func GetAccountByEmail(email string) (Account, error)  {
+	var account Account
+
+	return account, nil
+}
+
+func GetAccountByPhone(country int, phone string) (Account, error)  {
+	var account Account
+
+
+	return account, nil
+}
+
+func SetEmail(uid int64, email string) error {
+
+	return nil
+}
+
+func SetPhone(uid int64, country int, phone string) error {
+
+	return nil
+}
+func SetLoginPassword(uid int64, password string)  error {
+
+	return nil
+}
+
+func SetPaymentPassword(uid int64, password string) error {
+
+	return nil
 }
