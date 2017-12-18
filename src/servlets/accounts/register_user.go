8a71fe4ba97e9610db895c7331b16cfd35978462
lideyong <lideyong@outlook.com>
2017-12-18 14:46:53 +0800

    update: register logic

--- src/servlets/accounts/register_user.go
+++ src/servlets/accounts/register_user.go
@@ -6,8 +6,8 @@ import (
 	"servlets/constants"
 	"strconv"
 	"time"
-	"utils/logger"
 	"utils"
+	"utils/logger"
 )
 
 // registerParam holds the request "param" field
@@ -61,7 +61,9 @@ func (handler *registerUserHandler) Handle(request *http.Request, writer http.Re
 
 	handler.header = common.ParseHttpHeaderParams(request)
 	common.ParseHttpBodyParams(request, &handler.registerData)
+
 	if handler.checkRequestParams() == false {
+		handler.setResponseBase(constants.RC_PARAM_ERR)
 		return
 	}
 
@@ -70,13 +72,30 @@ func (handler *registerUserHandler) Handle(request *http.Request, writer http.Re
 
 	account := handler.getAccount()
 
-	// newtoken, err := token.New(uid, "key", 24*3600)
+	var err error
 
 	switch handler.registerData.Param.Type {
 	case constants.LOGIN_TYPE_UID:
-		common.InsertAccount(account)
+		_, err = common.InsertAccount(account)
 	case constants.LOGIN_TYPE_EMAIL:
+		if common.ExistsEmail(account.Email) {
+			handler.setResponseBase(constants.RC_DUP_EMAIL)
+			return
+		} else {
+			_, err = common.InsertAccountWithEmail(account)
+		}
 	case constants.LOGIN_TYPE_PHONE:
+		if common.ExistsPhone(account.Country, account.Phone) {
+			handler.setResponseBase(constants.RC_DUP_PHONE)
+			return
+		} else {
+			_, err = common.InsertAccountWithPhone(account)
+		}
+	}
+
+	if err != nil {
+		handler.setResponseBase(constants.RC_SYSTEM_ERR)
+		return
 	}
 
 	handler.response.Data = &responseRegister{
@@ -85,39 +104,34 @@ func (handler *registerUserHandler) Handle(request *http.Request, writer http.Re
 	}
 }
 
+func (handler *registerUserHandler) setResponseBase(error constants.Error) {
+	handler.response.Base.RC = error.Rc
+	handler.response.Base.Msg = error.Msg
+	logger.Info(error.Msg)
+}
+
 func (handler *registerUserHandler) checkRequestParams() bool {
-	// if handler.header.Timestamp < 1 {
-	// 	handler.response.Base.RC = constants.RC_READ_REQUEST_PARAM_ERROR
-	// 	handler.response.Base.Msg = "read http request params error"
-	// 	return false
-	// }
+	if handler.header.Timestamp < 1 {
+		return false
+	}
 
-	// if (handler.registerData.Base.App == nil) || (handler.registerData.Base.App.IsValid() == false) {
-	// 	handler.response.Base.RC = constants.RC_READ_REQUEST_PARAM_ERROR
-	// 	handler.response.Base.Msg = "read http request params error"
-	// 	return false
-	// }
+	if (handler.registerData.Base.App == nil) || (handler.registerData.Base.App.IsValid() == false) {
+		return false
+	}
 
-	// if (handler.registerData.Param.Type < constants.LOGIN_TYPE_UID) && (handler.registerData.Param.Type > constants.LOGIN_TYPE_PHONE) {
-	// 	handler.response.Base.RC = constants.RC_READ_REQUEST_PARAM_ERROR
-	// 	handler.response.Base.Msg = "read http request params error"
-	// 	return false
-	// }
+	if (handler.registerData.Param.Type < constants.LOGIN_TYPE_UID) && (handler.registerData.Param.Type > constants.LOGIN_TYPE_PHONE) {
+		return false
+	}
 
-	// if (len(handler.registerData.Param.PWD) < 1) && (handler.registerData.Param.Spkv < 1) {
-	// 	handler.response.Base.RC = constants.RC_READ_REQUEST_PARAM_ERROR
-	// 	handler.response.Base.Msg = "read http request params error"
-	// 	return false
-	// }
+	if handler.registerData.Param.Type == constants.LOGIN_TYPE_EMAIL && len(handler.registerData.Param.EMail) < 1 {
+		return false
+	}
 
-	if (handler.header.Timestamp < 1) ||
-		(handler.registerData.Base.App == nil) || (handler.registerData.Base.App.IsValid() == false) ||
-		(handler.registerData.Param.Type < constants.LOGIN_TYPE_UID) && (handler.registerData.Param.Type > constants.LOGIN_TYPE_PHONE) ||
-		(len(handler.registerData.Param.PWD) < 1) && (handler.registerData.Param.Spkv < 1) {
+	if handler.registerData.Param.Type == constants.LOGIN_TYPE_PHONE && (handler.registerData.Param.Country == 0 || len(handler.registerData.Param.Phone) < 1) {
+		return  false
+	}
 
-		handler.response.Base.RC = constants.RC_PARAM_ERR.Rc
-		handler.response.Base.Msg = constants.RC_PARAM_ERR.Msg
-		logger.Info("request params error")
+	if (len(handler.registerData.Param.PWD) < 1) || (handler.registerData.Param.Spkv < 1) {
 		return false
 	}
 
@@ -126,15 +140,26 @@ func (handler *registerUserHandler) checkRequestParams() bool {
 
 func (handler *registerUserHandler) getAccount() common.Account {
 	var account common.Account
+	var uid string
+	var uid_num int64
 
-	uid := common.GenerateUID(9)
+	for {
+		uid = common.GenerateUID(9)
+		uid_num, _ = strconv.ParseInt(uid, 10, 64)
+
+		if common.ExistsUID(uid_num) {
+			continue
+		} else {
+			break
+		}
+	}
 
 	account.UIDString = uid
-	account.UID, _ = strconv.ParseInt(uid, 10, 64)
+	account.UID = uid_num
 
-	//account.Email = handler.registerData.Param.EMail
-	//account.Country = handler.registerData.Param.Country
-	//account.Phone = handler.registerData.Param.Phone
+	account.Email = handler.registerData.Param.EMail
+	account.Country = handler.registerData.Param.Country
+	account.Phone = handler.registerData.Param.Phone
 
 	account.LoginPassword = utils.Sha256(handler.registerData.Param.PWD + uid)
 	account.RegisterTime = time.Now().Unix()
