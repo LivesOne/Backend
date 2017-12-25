70e7b8f60cbfb493a94d7e902143aad7bce5d7ee
KevinLVT <zhangrongkang@maxthon.net>
2017-12-25 18:12:19 +0800

    add logout

--- src/servlets/accounts/logout.go
+++ src/servlets/accounts/logout.go
@@ -4,17 +4,19 @@ import (
 	"net/http"
 	"servlets/common"
 	"servlets/constants"
+	"servlets/token"
+	"utils/logger"
 )
 
 type logoutRequest struct {
-	Base  common.BaseInfo `json:"base"`
-	Param string          `json:"param"`
+	Base  *common.BaseInfo `json:"base"`
+	Param string           `json:"param"`
 }
 
 // logoutHandler implements the "Echo message" interface
 type logoutHandler struct {
-	header    *common.HeaderParams // request header param
-	logouData *logoutRequest       // request login data
+	header     *common.HeaderParams // request header param
+	logoutData *logoutRequest       // request login data
 }
 
 func (handler *logoutHandler) Method() string {
@@ -23,14 +25,56 @@ func (handler *logoutHandler) Method() string {
 
 func (handler *logoutHandler) Handle(request *http.Request, writer http.ResponseWriter) {
 
-	response := &common.ResponseData{
-		Base: &common.BaseResp{
-			RC:  constants.RC_OK.Rc,
-			Msg: constants.RC_OK.Msg,
-		},
-	}
+	response := common.NewResponseData()
 	defer common.FlushJSONData2Client(response, writer)
 
 	handler.header = common.ParseHttpHeaderParams(request)
-	common.ParseHttpBodyParams(request, &handler.logouData)
+	common.ParseHttpBodyParams(request, &handler.logoutData)
+
+	if handler.checkRequestParams() == false {
+		response.SetResponseBase(constants.RC_PARAM_ERR)
+		return
+	}
+
+	errT := token.Del(handler.header.TokenHash)
+	if errT != constants.ERR_INT_OK {
+		logger.Info("logout: delete token failed")
+		response.SetResponseBase(constants.RC_INVALID_TOKEN)
+	}
 }
+
+func (handler *logoutHandler) checkRequestParams() bool {
+	if (handler.header == nil) || (handler.logoutData == nil) || (handler.header.IsValid() == false) {
+		return false
+	}
+
+	if (handler.logoutData.Base.App == nil) || (handler.logoutData.Base.App.IsValid() == false) {
+		return false
+	}
+
+	if len(handler.logoutData.Param) < 1 {
+		return false
+	}
+
+	return true
+}
+
+// func (handler *logoutHandler) getHashedToken(tokenUpload string) string {
+
+// 	aesKey, errT := token.GetKey(handler.header.TokenHash)
+// 	if errT != constants.ERR_INT_OK {
+// 		logger.Info("autologin: get uid from token cache failed")
+// 		return ""
+// 	}
+
+// 	iv := aesKey[:constants.AES_ivLen]
+// 	key := aesKey[constants.AES_ivLen:]
+// 	tokenTmp := utils.Base64Decode(tokenUpload)
+// 	tokenDecrypt, err := utils.AesDecrypt(string(tokenTmp), string(key), string(iv))
+// 	if err != nil {
+// 		logger.Info("autologin: parse token failed", tokenUpload)
+// 		return ""
+// 	}
+
+// 	return utils.Sha256(string(tokenDecrypt))
+// }
