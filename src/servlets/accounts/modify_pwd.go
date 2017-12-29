d0fbbf4b9caaf7b28ac9b683d9720dc4251894e5
KevinLVT <zhangrongkang@maxthon.net>
2017-12-29 17:49:29 +0800

    modify pwd

--- src/servlets/accounts/modify_pwd.go
+++ src/servlets/accounts/modify_pwd.go
@@ -6,6 +6,7 @@ import (
 	"servlets/constants"
 	"servlets/token"
 	"utils"
+	"utils/logger"
 )
 
 const (
@@ -24,14 +25,11 @@ type modifySecret struct {
 }
 
 type modifyPwdRequest struct {
-	// Base  common.BaseInfo `json:"base"`
 	Param modifyPwdParam `json:"param"`
 }
 
 // modifyPwdHandler
 type modifyPwdHandler struct {
-	// header      *common.HeaderParams // request header param
-	// requestData *modifyPwdRequest    // request body
 }
 
 func (handler *modifyPwdHandler) Method() string {
@@ -47,7 +45,12 @@ func (handler *modifyPwdHandler) Handle(request *http.Request, writer http.Respo
 	requestData := new(modifyPwdRequest)
 	common.ParseHttpBodyParams(request, &requestData)
 
-	if httpHeader.Timestamp < 1 {
+	// if httpHeader.IsValid() == false {
+	if (httpHeader.IsValidTimestamp() == false) ||
+		(httpHeader.IsValidTokenhash() == false) ||
+		((requestData.Param.Type != LOGIN_PASSWORD) && (requestData.Param.Type != PAYMENT_PASSWORD)) ||
+		(len(requestData.Param.Secret) < 1) {
+		logger.Info("modify pwd: request param error")
 		response.SetResponseBase(constants.RC_PARAM_ERR)
 		return
 	}
@@ -55,97 +58,89 @@ func (handler *modifyPwdHandler) Handle(request *http.Request, writer http.Respo
 	// 判断用户身份
 	uidString, aesKey, _, tokenErr := token.GetAll(httpHeader.TokenHash)
 	if err := TokenErr2RcErr(tokenErr); err != constants.RC_OK {
+		logger.Info("modify pwd: get info from cache error:", err)
 		response.SetResponseBase(err)
+		return
 	}
-	uid := utils.Str2Int64(uidString)
+	if len(aesKey) != constants.AES_totalLen {
+		logger.Info("modify pwd: get aeskey from cache error:", len(aesKey))
+		response.SetResponseBase(constants.RC_SYSTEM_ERR)
+		return
+	}
 
 	// 解码 secret 参数
-	secretString := requestData.Param.Secret
+	// secretString := requestData.Param.Secret
 	secret := new(modifySecret)
 	iv, key := aesKey[:constants.AES_ivLen], aesKey[constants.AES_ivLen:]
-	if err := DecryptSecret(secretString, key, iv, &secret); err != constants.RC_OK {
-		response.SetResponseBase(err)
+	errT := DecryptSecret(requestData.Param.Secret, key, iv, &secret)
+	if (errT != constants.RC_OK) || (len(secret.NewPwd) < 1) {
+		logger.Info("modify pwd: decrypt secret error:", errT, len(secret.NewPwd))
+		response.SetResponseBase(errT)
+		return
 	}
-
-	// 解析出“sha256(密码)”
-	// 数据库实际保存的密码格式为“sha256(sha256(密码) + uid)”
-	pwdDb := utils.Sha256(secret.Pwd + uidString)
-	newPwdDb := utils.Sha256(secret.NewPwd + uidString)
-
-	// 判断各种参数不合法的情况
-	if secret.NewPwd == "" {
+	// 检查新旧密码是否重复
+	if secret.Pwd == secret.NewPwd {
+		logger.Info("modify pwd: orginal password equal to new password in request param")
 		response.SetResponseBase(constants.RC_PARAM_ERR)
 		return
 	}
+
+	account, err := common.GetAccountByUID(uidString)
+	if err != nil {
+		logger.Info("modify pwd: get account info from DB error:", err)
+		response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
+		return
+	}
+
+	uid := utils.Str2Int64(uidString)
+
+	// 解析出“sha256(密码)”
+	// 数据库实际保存的密码格式为“sha256(sha256(密码) + uid)”
+	newPwdDb := utils.Sha256(secret.NewPwd + uidString)
 
 	modifyType := requestData.Param.Type
 	if modifyType == LOGIN_PASSWORD {
 		// 检查密码为空
 		if secret.Pwd == "" {
-			response.SetResponseBase(constants.RC_PARAM_ERR)
-			return
-		}
-		// 检查新旧密码是否重复
-		if secret.Pwd == secret.NewPwd {
+			logger.Info("modify pwd: original pwd is empty")
 			response.SetResponseBase(constants.RC_PARAM_ERR)
 			return
 		}
 		// check old password
-		account, err := common.GetAccountByUID(uidString)
-		if err != nil {
-			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
-			return
-		}
+		pwdDb := utils.Sha256(secret.Pwd + uidString)
 		if account.LoginPassword != pwdDb {
+			logger.Info("modify pwd: orginal login password in DB not equal to new password")
 			response.SetResponseBase(constants.RC_INVALID_LOGIN_PWD)
 			return
 		}
 		// save to db
 		if err := common.SetLoginPassword(uid, newPwdDb); err != nil {
+			logger.Info("modify pwd: save new login pwd to DB error", err)
 			response.SetResponseBase(constants.RC_SYSTEM_ERR)
 		}
-		// send response
-		response.SetResponseBase(constants.RC_OK)
-		return
 
 	} else if modifyType == PAYMENT_PASSWORD {
 		if secret.Pwd == "" {
+			// first time of set payment password
 			// 检查交易密码是否被设置过
-			account, err := common.GetAccountByUID(uidString)
-			if err != nil {
-				response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
-				return
-			}
 			if account.PaymentPassword != "" {
-				response.SetResponseBase(constants.RC_INVALID_LOGIN_PWD)
+				logger.Info("modify pwd: you have set payment pwd before")
+				response.SetResponseBase(constants.RC_PARAM_ERR)
 				return
 			}
 		} else {
 			// check old password
-			if secret.NewPwd != secret.Pwd {
-				response.SetResponseBase(constants.RC_INVALID_LOGIN_PWD)
-				return
-			}
-			account, err := common.GetAccountByUID(uidString)
-			if err != nil {
-				response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
-				return
-			}
+			pwdDb := utils.Sha256(secret.Pwd + uidString) //原密码哈希，首次设置交易密码此处为空
 			if account.PaymentPassword != pwdDb {
+				logger.Info("modify pwd: orginal payment password in DB not equal to new password")
 				response.SetResponseBase(constants.RC_INVALID_PAYMENT_PWD)
 				return
 			}
 		}
 		// save to db
 		if err := common.SetPaymentPassword(uid, newPwdDb); err != nil {
+			logger.Info("modify pwd: save new payment pwd to DB error", err)
 			response.SetResponseBase(constants.RC_SYSTEM_ERR)
 		}
-		// send response
-		response.SetResponseBase(constants.RC_OK)
-		return
-
-	} else {
-		response.SetResponseBase(constants.RC_PARAM_ERR)
-		return
 	}
 }
