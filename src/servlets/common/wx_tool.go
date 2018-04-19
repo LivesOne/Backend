72db8585eb43f0d0f727f23e19b53db85e01b1c4
lrs <82623629@qq.com>
2018-04-19 16:35:44 +0800

    增加微信错误返回的校验

--- src/servlets/common/wx_tool.go
+++ src/servlets/common/wx_tool.go
@@ -1,10 +1,10 @@
 package common
 
 import (
-	"utils/config"
-	"utils/lvthttp"
-	"utils/logger"
 	"utils"
+	"utils/config"
+	"utils/logger"
+	"utils/lvthttp"
 )
 
 type (
@@ -15,30 +15,47 @@ type (
 		Scope       string `json:"scope"`
 		Unionid     string `json:"unionid"`
 	}
+	//{"errcode":41002,"errmsg":"appid missing, hints: [ req_id: a_ZoNA0774th54 ]"}
+	wxErrorRes struct {
+		Errcode int    `json:"errcode"`
+		Errmsg  string `json:"errmsg"`
+	}
 )
 
-
-
-func AuthWX(code string)(bool,*wxRes){
+func AuthWX(code string) (bool, *wxRes) {
 
 	wx := config.GetConfig().WXAuth
 
-	param := make(map[string]string,0)
+	param := make(map[string]string, 0)
 	param["appid"] = wx.Appid
 	param["secret"] = wx.Secret
 	param["code"] = code
 	param["grant_type"] = "authorization_code"
 
-	resBody,err := lvthttp.Get(wx.Url,param)
+	resBody, err := lvthttp.Get(wx.Url, param)
 	if err != nil {
-		logger.Error("http req error",err.Error())
-		return false,nil
+		logger.Error("http req error", err.Error())
+		return false, nil
 	}
-	logger.Info("wx http res ",resBody)
+	logger.Info("wx http res ", resBody)
+
+	//校验是否是错误返回的格式
+	errRes := new(wxErrorRes)
+	if err = utils.FromJson(resBody, errRes); err != nil {
+		logger.Error("json parse error", err.Error(), "res body", resBody)
+		return false, nil
+	}
+
+	if errRes.Errcode > 0 {
+		logger.Error("wx req error",errRes.Errmsg)
+		return false, nil
+	}
+
+
 	res := new(wxRes)
-	if err = utils.FromJson(resBody,res);err != nil {
-		logger.Error("json parse error",err.Error(),"res body",resBody)
-		return false,nil
+	if err = utils.FromJson(resBody, res); err != nil {
+		logger.Error("json parse error", err.Error(), "res body", resBody)
+		return false, nil
 	}
-	return true,res
+	return true, res
 }
