f601f1f34470000d0691a94160715cc1fc60b94a
lrs <82623629@qq.com>
2018-05-08 15:04:12 +0800

    加入完全没有参数时 不校验

--- src/servlets/accounts/upgrade.go
+++ src/servlets/accounts/upgrade.go
@@ -17,7 +17,7 @@ type upgradeParam struct {
 }
 
 type upgradeRequest struct {
-	Param upgradeParam `json:"param"`
+	Param *upgradeParam `json:"param"`
 }
 
 type upgradeResData struct {
@@ -63,47 +63,41 @@ func (handler *upgradeHandler) Handle(request *http.Request, writer http.Respons
 
 	requestData := new(upgradeRequest)
 
-	if  !common.ParseHttpBodyParams(request, requestData) {
-		response.SetResponseBase(constants.RC_PROTOCOL_ERR)
-		return
-	}
-
-
-
-	// 解码 secret 参数
-	secretString := requestData.Param.Secret
-	secret := new(upgradeSecret)
-	iv, key := aesKey[:constants.AES_ivLen], aesKey[constants.AES_ivLen:]
-	if err := DecryptSecret(secretString, key, iv, secret); err != constants.RC_OK {
-		response.SetResponseBase(err)
-		return
-	}
-
-	if len(secret.WxCode) > 0 {
-		// 微信二次验证
-		uid := utils.Str2Int64(uidString)
-		//未绑定返回验升级失败
-		openId,unionId,_ := common.GetUserExtendByUid(uid)
-		if len(openId) == 0 || len(unionId) == 0 {
-			response.SetResponseBase(constants.RC_UPGRAD_FAILED)
+	common.ParseHttpBodyParams(request, requestData)
+	//判断有合法参数才进行微信二次校验
+	if requestData.Param != nil && len(requestData.Param.Secret) >0 {
+		// 解码 secret 参数
+		secretString := requestData.Param.Secret
+		secret := new(upgradeSecret)
+		iv, key := aesKey[:constants.AES_ivLen], aesKey[constants.AES_ivLen:]
+		if err := DecryptSecret(secretString, key, iv, secret); err != constants.RC_OK {
+			response.SetResponseBase(err)
 			return
 		}
-		//微信认证并比对id
-		if ok,res := common.AuthWX(secret.WxCode);ok {
-			if res.Unionid != unionId || res.Openid != openId {
-				response.SetResponseBase(constants.RC_WX_SEC_AUTH_FAILED)
+
+		if len(secret.WxCode) > 0 {
+			// 微信二次验证
+			uid := utils.Str2Int64(uidString)
+			//未绑定返回验升级失败
+			openId,unionId,_ := common.GetUserExtendByUid(uid)
+			if len(openId) == 0 || len(unionId) == 0 {
+				response.SetResponseBase(constants.RC_UPGRAD_FAILED)
 				return
 			}
-		} else {
-			response.SetResponseBase(constants.RC_INVALID_WX_CODE)
-			return
+			//微信认证并比对id
+			if ok,res := common.AuthWX(secret.WxCode);ok {
+				if res.Unionid != unionId || res.Openid != openId {
+					response.SetResponseBase(constants.RC_WX_SEC_AUTH_FAILED)
+					return
+				}
+			} else {
+				response.SetResponseBase(constants.RC_INVALID_WX_CODE)
+				return
+			}
+
 		}
 
 	}
-
-
-
-
 
 	if ok,level := common.UserUpgrade(uidString);ok {
 		response.Data = upgradeResData{
