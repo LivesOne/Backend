0c1aa3ec9e9b2f4f46fcb597d9eafb9d87667047
lrs <82623629@qq.com>
2018-05-14 14:50:26 +0800

    修改错误的验证顺序

--- src/servlets/accounts/reset_pwd.go
+++ src/servlets/accounts/reset_pwd.go
@@ -22,8 +22,8 @@ type resetPwdParam struct {
 }
 
 type resetPwdRequest struct {
-	Base  common.BaseInfo `json:"base"`
-	Param resetPwdParam   `json:"param"`
+	Base  *common.BaseInfo `json:"base"`
+	Param *resetPwdParam   `json:"param"`
 }
 
 // resetPwdHandler
@@ -44,8 +44,14 @@ func (handler *resetPwdHandler) Handle(request *http.Request, writer http.Respon
 	requestData := resetPwdRequest{}
 	common.ParseHttpBodyParams(request, &requestData)
 
-	if header.IsValidTimestamp() == false {
+	if !header.IsValidTimestamp() {
 		log.Info("reset password: invalid timestamp")
+		response.SetResponseBase(constants.RC_PARAM_ERR)
+		return
+	}
+
+	param := requestData.Param
+	if param == nil {
 		response.SetResponseBase(constants.RC_PARAM_ERR)
 		return
 	}
@@ -53,62 +59,80 @@ func (handler *resetPwdHandler) Handle(request *http.Request, writer http.Respon
 	var account *common.Account
 	var err error
 	// 检查验证码
-	checkType := requestData.Param.Type
+	checkType := param.Type
 	switch checkType {
 	case 1: //邮箱验证
-		if !utils.IsValidEmailAddr(requestData.Param.EMail) {
+		if !utils.IsValidEmailAddr(param.EMail) {
 			log.Info("reset password: invalid email address")
 			response.SetResponseBase(constants.RC_PARAM_ERR)
 			return
 		}
+		if len(param.VCode) == 0 || len(param.VCodeID) == 0 {
+			log.Info("reset password: vcode is null or vcode_id is null")
+			response.SetResponseBase(constants.RC_PARAM_ERR)
+			return
+		}
 
-		if ok, errT := vcode.ValidateMailVCode(requestData.Param.VCodeID, requestData.Param.VCode, requestData.Param.EMail); !ok {
+		account, err = common.GetAccountByEmail(param.EMail)
+		if (err != nil) || (account == nil) {
+			log.Info("reset password: get account info by email failed:", param.EMail)
+			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
+			return
+		}
+		if ok, errT := vcode.ValidateMailVCode(param.VCodeID, param.VCode, account.Email); !ok {
 			log.Info("reset password: verify email vcode failed", errT)
 			response.SetResponseBase(vcode.ConvImgErr(errT))
 			return
 		}
 
-		account, err = common.GetAccountByEmail(requestData.Param.EMail)
-		if (err != nil) || (account == nil) {
-			log.Info("reset password: get account info by email failed:", requestData.Param.EMail)
-			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
-			return
-		}
+
 	case 2: //短信下行验证
-		if (len(requestData.Param.Phone) < 1) || (requestData.Param.Country < 1) {
-			log.Info("reset password: invalid phone or country", requestData.Param.Country, requestData.Param.Phone)
+		if (len(param.Phone) < 1) || (param.Country < 1) {
+			log.Info("reset password: invalid phone or country", param.Country, param.Phone)
 			response.SetResponseBase(constants.RC_PARAM_ERR)
 			return
 		}
-		if ok, err := vcode.ValidateSmsAndCallVCode(requestData.Param.Phone, requestData.Param.Country, requestData.Param.VCode, 0, 0); !ok {
+		if len(param.VCode) == 0 || len(param.VCodeID) == 0 {
+			log.Info("reset password: vcode is null or vcode_id is null")
+			response.SetResponseBase(constants.RC_PARAM_ERR)
+			return
+		}
+		account, err = common.GetAccountByPhone(param.Country, param.Phone)
+		if (err != nil) || (account == nil) {
+			log.Info("reset password: get account info by phone failed:", param.Country, param.Phone)
+			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
+			return
+		}
+		if ok, err := vcode.ValidateSmsAndCallVCode(account.Phone, account.Country, param.VCode, 0, 0); !ok {
 			e := vcode.ConvSmsErr(err)
 			log.Info("reset password: verify sms vcode failed", e)
 			response.SetResponseBase(e)
 			return
 		}
-		account, err = common.GetAccountByPhone(requestData.Param.Country, requestData.Param.Phone)
-		if (err != nil) || (account == nil) {
-			log.Info("reset password: get account info by phone failed:", requestData.Param.Country, requestData.Param.Phone)
-			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
-			return
-		}
+
 	case 3: //短信上行验证
-		if (len(requestData.Param.Phone) < 1) || (requestData.Param.Country < 1) {
-			log.Info("reset password: invalid phone or country", requestData.Param.Country, requestData.Param.Phone)
+		if (len(param.Phone) < 1) || (param.Country < 1) {
+			log.Info("reset password: invalid phone or country", param.Country, param.Phone)
 			response.SetResponseBase(constants.RC_PARAM_ERR)
 			return
 		}
-		if ok, resErr := vcode.ValidateSmsUpVCode(requestData.Param.Country, requestData.Param.Phone, requestData.Param.VCode); !ok {
+		if len(param.VCode) == 0 {
+			log.Info("reset password: vcode is null")
+			response.SetResponseBase(constants.RC_PARAM_ERR)
+			return
+		}
+		account, err = common.GetAccountByPhone(param.Country, param.Phone)
+		if (err != nil) || (account == nil) {
+			log.Info("reset password: get account info by phone failed:", param.Country, param.Phone)
+			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
+			return
+		}
+		if ok, resErr := vcode.ValidateSmsUpVCode(account.Country, account.Phone, param.VCode); !ok {
 			log.Info("validate up sms code failed")
 			response.SetResponseBase(resErr)
 			return
 		}
-		account, err = common.GetAccountByPhone(requestData.Param.Country, requestData.Param.Phone)
-		if (err != nil) || (account == nil) {
-			log.Info("reset password: get account info by phone failed:", requestData.Param.Country, requestData.Param.Phone)
-			response.SetResponseBase(constants.RC_INVALID_ACCOUNT)
-			return
-		}
+
 	default:
 		response.SetResponseBase(constants.RC_PARAM_ERR)
 		return
@@ -116,13 +140,13 @@ func (handler *resetPwdHandler) Handle(request *http.Request, writer http.Respon
 	}
 
 	// 解析出“sha256(密码)”
-	privKey, err := config.GetPrivateKey(requestData.Param.Spkv)
+	privKey, err := config.GetPrivateKey(param.Spkv)
 	if (err != nil) || (privKey == nil) {
 		log.Error("can not get private key")
 		response.SetResponseBase(constants.RC_PARAM_ERR)
 		return
 	}
-	pwdSha256, err := utils.RsaDecrypt(requestData.Param.PWD, privKey)
+	pwdSha256, err := utils.RsaDecrypt(param.PWD, privKey)
 	if err != nil {
 		log.Info("reset password: decrypt pwd error:", err)
 		response.SetResponseBase(constants.RC_SYSTEM_ERR)
