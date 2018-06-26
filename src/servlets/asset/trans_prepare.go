cf5c304409506278382be7cbede0613191ebd6f8
lrs <82623629@qq.com>
2018-06-26 16:22:43 +0800

    加入日志

--- src/servlets/asset/trans_prepare.go
+++ src/servlets/asset/trans_prepare.go
@@ -148,6 +148,7 @@ func (handler *transPrepareHandler) Handle(request *http.Request, writer http.Re
 	to := utils.Str2Int64(secret.To)
 	txType := requestData.Param.TxType
 
+	log.Debug(from,to,txType)
 	//chacke to
 	switch txType {
 	case constants.TX_TYPE_BUY_COIN_CARD:
@@ -160,7 +161,6 @@ func (handler *transPrepareHandler) Handle(request *http.Request, writer http.Re
 		}
 	}
 
-	log.Debug(from,to,txType)
 	//交易类型 只支持，红包，转账，购买，退款 不支持私募，工资
 	switch txType {
 	case constants.TX_TYPE_TRANS:
