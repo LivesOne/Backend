d5f31bffbab6c3a1b0838a68c8215b7f1cb7b597
lrs <82623629@qq.com>
2018-11-13 16:45:21 +0800

    update: add/find contacts

--- src/servlets/contacts/register.go
+++ src/servlets/contacts/register.go
@@ -10,4 +10,6 @@ func RegisterHandlers() {
 	server.RegisterHandler(constants.ACCOUNT_CONTACTS_CREATE, new(contactCreateHandler))
 	server.RegisterHandler(constants.ACCOUNT_CONTACTS_MODIFY, new(contactModifyHandler))
 	server.RegisterHandler(constants.ACCOUNT_CONTACTS_DELETE, new(contactDeleteHandler))
+	server.RegisterHandler(constants.ACCOUNT_CONTACTS_FIND, new(contactFindHandler))
+	server.RegisterHandler(constants.ACCOUNT_CONTACTS_ADD, new(contactAddHandler))
 }
