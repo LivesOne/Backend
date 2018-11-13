bc2be998026695d6aaf5a15d2a2d7bd6f3b3bdfd
lrs <82623629@qq.com>
2018-11-13 19:27:18 +0800

    fixed: remove json encode

--- src/servlets/accounts/get_user_profile.go
+++ src/servlets/accounts/get_user_profile.go
@@ -11,28 +11,28 @@ import (
 )
 
 type profileResponse struct {
-	Uid            string           `json:"uid,omitempty"`
-	Nickname       string           `json:"nickname,omitempty"`
-	Email          string           `json:"email,omitempty"`
-	Country        int64            `json:"country,omitempty"`
-	Phone          string           `json:"phone,omitempty"`
-	HavePayPwd     bool             `json:"have_pay_pwd,omitempty"`
-	TransLevel     int              `json:"trans_level,omitempty"`
-	BindWx         bool             `json:"bind_wx,omitempty"`
-	CreditScore    int64            `json:"credit_score,omitempty"`
-	BindTg         bool             `json:"bind_tg,omitempty"`
-	WalletAddress  string           `json:"wallet_address,omitempty"`
-	AvatarUrl      string           `json:"avatar_url,omitempty"`
-	ActiveDays     int64            `json:"active_days,omitempty"`
-	Level          int64            `json:"level,omitempty"`
-	UpdateTime     int64            `json:"update_time,omitempty"`
-	RegisterTime   int64            `json:"register_time,omitempty"`
-	HashrateDetial []hashrateDetial `json:"hashrate,omitempty"`
+	Uid            string           `json:"uid"`
+	Nickname       string           `json:"nickname"`
+	Email          string           `json:"email"`
+	Country        int64            `json:"country"`
+	Phone          string           `json:"phone"`
+	HavePayPwd     bool             `json:"have_pay_pwd"`
+	TransLevel     int              `json:"trans_level"`
+	BindWx         bool             `json:"bind_wx"`
+	CreditScore    int64            `json:"credit_score"`
+	BindTg         bool             `json:"bind_tg"`
+	WalletAddress  string           `json:"wallet_address"`
+	AvatarUrl      string           `json:"avatar_url"`
+	ActiveDays     int64            `json:"active_days"`
+	Level          int64            `json:"level"`
+	UpdateTime     int64            `json:"update_time"`
+	RegisterTime   int64            `json:"register_time"`
+	HashrateDetial []hashrateDetial `json:"hashrate"`
 }
 
 type hashrateDetial struct {
-	Type  int `json:"type,omitempty"`
-	Value int `json:"value,omitempty"`
+	Type  int `json:"type"`
+	Value int `json:"value"`
 }
 
 // getProfileHandler
